400bf23e7f2ad99104d0bdb2a1b3a071bcdaa209
Tim Potts <timothy.potts@colorado.edu>
2019-10-16 13:14:46 -0600

    got a working score keeper and the questions and prompts progress at the same rate

--- script.js
+++ script.js
@@ -33,11 +33,28 @@ var questions =
     }
 ];
 
+var qindex = ["q1","q2","q3","q4"];
+
+// initialize points
+var score = 0;
+var round = 0;
+
+// Start timer and populate page with prompt and answers
 document.getElementById("start").addEventListener("click", function () {
     // initialize button elements
     var parent = document.getElementById("start").parentNode;
     var vanish = document.getElementById("start");
 
+    //start timer:
+
+
+    // initialize prompt element
+    var promptparent = document.getElementById("promptspace").parentNode;
+    var node = document.createElement("H2");
+    var textnode = document.createTextNode(questions[round].title);
+    node.setAttribute("id", "prompt");
+    node.appendChild(textnode);
+    promptparent.appendChild(node);
 
     // remove the start button
     parent.removeChild(vanish);
@@ -45,21 +62,50 @@ document.getElementById("start").addEventListener("click", function () {
     // populate page with multiple choice elements
     for(var i=0; i<4; i++) {
         var nbut = document.createElement("BUTTON");
+        var quest = document.createTextNode(questions[round].choices[i]);
+        nbut.appendChild(quest);
+        parent.appendChild(nbut);
+        nbut.classList.add("choices");
         nbut.classList.add("btn");
         nbut.classList.add("btn-primary");
         nbut.classList.add("m-2");
-        var quest = document.createTextNode(questions[0].choices[i]);
-        nbut.appendChild(quest);
-        parent.appendChild(nbut);
+        nbut.setAttribute("id", qindex[i]);
+        nbut.setAttribute("onclick","nextRound(this)");
     }
-
+    round++;
+    return round;
 });
 
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
 
-
-console.log(questions[1].choices[0]);
-
-
+// Moves questions on to next set and keeps score
+function nextRound(elem) {
+    if(elem.textContent==questions[round-1].answer) {
+        score++;
+        console.log(score)
+    }
+    if(round===5){
+        document.getElementById("prompt").textContent="Your final score is: " + score;
+        for (i=0; i<4; i++) {
+            document.getElementById(qindex[i]).remove();
+        }
+        return;
+    }
+    for (i=0; i<4; i++) {
+    document.getElementById("prompt").textContent=questions[round].title;
+    document.getElementById(qindex[i]).textContent=questions[round].choices[i];
+    }
+    round++
+}
 /* function countdown()
 {
     this.start_time = "1:15";
